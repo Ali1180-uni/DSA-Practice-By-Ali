3f0deb74adc535f6d42b8892cf30f3acbecd0ad3
Ali1180-uni <24ntucsfl1180@student.ntu.edu.pk>
2025-10-19 20:30:14 +0500

    Practice Added

---
+++ Linked List/hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Enter Your Age:"<<endl;
+	int num;
+	cin>>num;
+	if(num == 18){
+		cout<<"Website Data";
+	}else{
+		cout<<"Tum choty bachay ho abhi Free Fire khelo jakay"<<endl;
+	}
+}
--- Practice/Josephus Problem.cpp
+++ Practice/Josephus Problem.cpp
@@ -3,29 +3,47 @@ using namespace std;
 class Node{
 	public:
 		int data;
-		Node *Prev;
-		Node *Next;
+		Node *Next,*Prev;
 		Node(){
 			data = 0;
-			Prev = Next = nullptr;
+			Next = Prev = nullptr;
 		}
-}
+};
 Node *CirDbl(int n){
+	if(n<=0){
+		cout<<"Invalid"<<endl;
+		return nullptr;
+	}
 	Node *First = new Node();
 	First->data = 1;
 	Node *Last = First;
-	Node *Curr = First;
-	for(int i = 2; i<=n; i++){
+	for(int i = 2; i<n; i++){
 		Node *temp = new Node();
 		temp->data = i;
 		Last->Next = temp;
+		temp->Prev = Last;
 		Last = temp;
-		Last->Prev = Curr;
-		Curr = Curr->Next;
 	}
 	Last->Next = First;
+	First->Prev = Last;
 	return First;
 }
+int josephus(int n, int k){
+	Node *Head = CirDbl(n);
+	Node *Curr;
+	while(Head->Next != Head){
+		for(int i = 1; i<=k; i++){
+			Curr = Head;
+			Head = Head->Next;
+		}
+		Curr->Next = Head->Next;
+		Head = Head->Next;
+	}
+	int win = Head->data;
+	return win;
+}
+
 int main(){
-	
+	int winner = josephus(6,3);
+	cout<<"The King :"<<winner;
 }
