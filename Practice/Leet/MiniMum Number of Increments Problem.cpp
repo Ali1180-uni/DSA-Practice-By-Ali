982065bde2616d9e6c9bf81db09ad3b617538fcd
Ali1180-uni <24ntucsfl1180@student.ntu.edu.pk>
2025-10-30 20:23:57 +0500

    Comments added

--- Practice/Leet/MiniMum Number of Increments Problem.cpp
+++ Practice/Leet/MiniMum Number of Increments Problem.cpp
@@ -1,11 +1,40 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+// Method To Convert Segments
 int minNumberOperations(vector<int>& target) {
-	int count = target[0];
-	for(int i = 1; i < target.size(); i++) {
-		if(target[i] > target[i-1]) {
-			count += target[i] - target[i-1];
+	int n = target.size();
+	int initial[n];
+	int count = 0;
+	for(int i = 0; i<n; i++) {
+		initial[i] = 0;
+	}
+	while (1) {
+		for(int i = 0; i<n; i++) {
+			if(initial[i] != target[i]) {
+				if(initial[i] != target[i]) {
+					initial[i]++;
+				} else if(initial[i] == target[i]) {
+					break;
+				}
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			cout<<initial[i];
+		}
+		cout<<endl;
+		bool check = true;
+		for(int i = 0; i<n; i++) {
+			if(initial[i]!=target[i]) {
+				check = false;
+				break;
+			}
+		}
+		if(check == true) {
+			count++;
+			break;
+		} else {
+			count++;
 		}
 	}
 	return count;
@@ -14,5 +43,14 @@ int main() {
 	vector<int> arr1 = {1,2,3,2,1};
 	vector<int> arr2 = {3,1,5,4,2};
 	vector<int> arr3 = {3,1,1,2};
-	cout<<minNumberOperations(arr1);
+	cout<<minNumberOperations(arr3);
 }
+
+// Correct Method
+//int count = target[0];
+//for(int i = 1; i < target.size(); i++) {
+//	if(target[i] > target[i-1]) {
+//		count += target[i] - target[i-1];
+//	}
+//}
+//return count;
