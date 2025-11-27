1ad550ff46551ac853dc43fc910f7db7e80d86fb
Ali1180-uni <thegreat1552005@gmail.com>
2025-11-27 17:42:34 +0500

    Trees and its Traverals are Added

---
+++ Trees/Pre Order Tree.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+class Node {
+	public:
+		int data;
+		Node *left,*right;
+		Node() {
+			data = 0;
+			left = right = nullptr;
+		}
+};
+class binarySearchTree {
+	public:
+		Node *root;
+		binarySearchTree() {
+			root = nullptr;
+		}
+		void insert(int d) {
+			Node *temp = new Node();
+			temp->data = d;
+			if(root == nullptr) {
+				root = temp;
+				return;
+			} else {
+				Node *curr = root;
+				Node *parent;
+				while(curr != nullptr) {
+					parent = curr;
+					if(d>curr->data) {
+						curr = curr->right;
+					} else {
+						curr = curr->left;
+					}
+				}
+				if(d>parent->data) {
+					parent->right = temp;
+				} else {
+					parent->left = temp;
+				}
+			}
+		}
+		void PreOrder(Node *temp) {
+			if(temp == nullptr) {
+				return;
+			}
+			cout<<temp->data<<" ";
+			PreOrder(temp->left);
+			PreOrder(temp->right);
+		}
+};
+int main() {
+	binarySearchTree tree1;
+	tree1.insert(14);
+	tree1.insert(13);
+	tree1.insert(16);
+	tree1.insert(20);
+	tree1.insert(18);
+	cout<<"\n--------Pre Order--------\n";
+	tree1.PreOrder(tree1.root);
+}
