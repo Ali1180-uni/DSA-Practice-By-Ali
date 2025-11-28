155a9cd27a36460d77aaddb4d1f04505675ad787
Ali1180-uni <thegreat1552005@gmail.com>
2025-11-28 19:59:34 +0500

    Lab 7 Completed

--- Labs/Lab Plan 7/Q8 Sum of n Recursion.cpp
+++ Labs/Lab Plan 7/Q8 Sum of n Recursion.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int sum(int n){
-	int sum = 0;
-	for(int i = 1; i<=n; i++){
-		sum+=i;
+	if(n == 0){
+		return 0;
+	}else{
+		return n+sum(n-1);
 	}
-	return sum;
 }
 int main(){
 	int num;
