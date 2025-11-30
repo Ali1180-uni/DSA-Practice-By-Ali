b9d526d946661821252ec547d6f5b67c94ca2c07
Ali1180-uni <thegreat1552005@gmail.com>
2025-11-30 09:03:06 +0500

    Recursion Practice Added

--- Practice/Recursions/Power of 3.cpp
+++ Practice/Recursions/Power of 3.cpp
@@ -1,1 +1,15 @@
-s
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n == 0 || n == -1) {
+            return false;
+        }else if (n == 1 || n == 3) {
+            return true;
+        }else if(n % 3 != 0){
+            return false;
+        }
+        return isPowerOfThree(n/3);
+    }
+};
+
+thats the code which pass 21040 cases
