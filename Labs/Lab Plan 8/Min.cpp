c1e8a0931798169d8b0efe19745e0e6ae383440f
Ali1180-uni <thegreat1552005@gmail.com>
2025-12-19 11:34:35 +0500

    New Lab Added

---
+++ Labs/Lab Plan 8/Min.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+class Node {
+	public:
+		int data;
+		Node *left,*right;
+		Node() {
+			data = 0;
+			left = right = nullptr;
+		}
+};
+class BST {
+	public:
+		Node *root;
+		BST() {
+			root = nullptr;
+		}
+		void insert(int d) {
+			Node *temp = new Node();
+			temp->data = d;
+			if(root == nullptr) {
+				root = temp;
+				return;
+			} else {
+				Node *curr = root;
+				Node *prev;
+				while(curr!=nullptr) {
+					prev = curr;
+					if(d>curr->data) {
+						curr = curr->right;
+					} else {
+						curr = curr->left;
+					}
+				}
+				if(d>prev->data) {
+					prev->right = temp;
+				} else {
+					prev->left = temp;
+				}
+			}
+		}
+		void Min() {
+			Node *curr = root;
+			while(curr->left != nullptr) {
+				curr = curr->left;
+			}
+			cout<<"\nSmallest is: "<<curr->data<<endl;
+		}
+		void inorder(Node *temp) {
+			if(temp == nullptr) {
+				return;
+			}
+			inorder(temp->left);
+			cout<<" "<<temp->data;
+			inorder(temp->right);
+		}
+};
+int main() {
+	BST tree1;
+	tree1.insert(14);
+	tree1.insert(13);
+	tree1.insert(16);
+	tree1.insert(20);
+	tree1.insert(18);
+	tree1.Min();
+	cout<<"\n--------In Order--------\n";
+	tree1.inorder(tree1.root);
+
+}
---
+++ Labs/Lab Plan 8/Search.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+class Node {
+	public:
+		int data;
+		Node *left,*right;
+		Node() {
+			data = 0;
+			left = right = nullptr;
+		}
+};
+class BST {
+	public:
+		Node *root;
+		BST() {
+			root = nullptr;
+		}
+		void insert(int d) {
+			Node *temp = new Node();
+			temp->data = d;
+			if(root == nullptr) {
+				root = temp;
+				return;
+			} else {
+				Node *curr = root;
+				Node *prev;
+				while(curr!=nullptr) {
+					prev = curr;
+					if(d>curr->data) {
+						curr = curr->right;
+					} else {
+						curr = curr->left;
+					}
+				}
+				if(d>prev->data) {
+					prev->right = temp;
+				} else {
+					prev->left = temp;
+				}
+			}
+		}
+		bool Search(int num){
+			Node *curr = root;
+			if(root->data == num){
+				return true;
+			}
+			while(curr!=nullptr && curr->data != num){
+				if(num>curr->data){
+					curr = curr->right;
+				}else{
+					curr = curr->right;
+				}
+			}
+			if(curr == nullptr){
+				return false;
+			}else{
+				return true;
+			}
+		}
+		void inorder(Node *temp) {
+			if(temp == nullptr) {
+				return;
+			}
+			inorder(temp->left);
+			cout<<" "<<temp->data;
+			inorder(temp->right);
+		}
+};
+int main() {
+	BST tree1;
+	tree1.insert(14);
+	tree1.insert(13);
+	tree1.insert(16);
+	tree1.insert(20);
+	tree1.insert(18);
+	bool check = tree1.Search(20);
+	if(check == true){
+		cout<<"\nFound"<<endl;
+	}else{
+		cout<<"\nNot Found"<<endl;
+	}
+	cout<<"\n--------In Order--------\n";
+	tree1.inorder(tree1.root);
+
+}
